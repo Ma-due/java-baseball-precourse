857ee18e2b70fc31eadb026f33220b56bb4f5eba
Ma-due <maduee2@naver.com>
2021-11-08 22:58:57 +0900

    feat(OutputView): 게임 결과 처리

--- src/main/java/baseball/domain/game/GameController.java
+++ src/main/java/baseball/domain/game/GameController.java
@@ -3,29 +3,36 @@ package baseball.domain.game;
 
 import baseball.number.BaseballNumbers;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 import java.util.List;
 
 public class GameController {
 
-    private final BaseballNumbers targetNumber;
+    private final BaseballNumbers targetNumbers;
     private final InputView inputView;
 
-    private BaseballNumbers userNumber;
+    private BaseballNumbers userNumbers;
 
     public GameController(InputView inputview) {
-        this.targetNumber = BaseballNumbers.generateRandomNumbers();
+        this.targetNumbers = BaseballNumbers.generateRandomNumbers();
         this.inputView = inputview;
-
-        playGame();
     }
 
     private void playGame() {
         //숫자 입력
-        userNumber = BaseballNumbers.generateInputNumbers(inputView.inputNumber());
+        boolean exitCheck = false;
 
-        //점수 계산
+        while(!exitCheck) {
+            userNumbers = BaseballNumbers.generateInputNumbers(inputView.inputNumber());
 
-        //종료 확인
+            //점수 계산
+            int strikeCount = ScoreChecker.strikeCounting(targetNumbers, userNumbers);
+            int ballCount = ScoreChecker.ballCounting(targetNumbers, userNumbers);
+
+            //종료 확인
+            GameResult gameResult = new GameResult(ballCount, strikeCount);
+            exitCheck = OutputView.outMessage(gameResult);
+        }
     }
 }
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,29 @@
+package baseball.view;
+
+import baseball.domain.game.GameResult;
+
+public class OutputView {
+
+    public static boolean outMessage(GameResult gameResult) {
+        String resultMessage = getResultMessage(gameResult);
+        System.out.println(resultMessage);
+
+        return gameResult.isEndOfGame();
+    }
+
+    private static String getResultMessage(GameResult gameResult) {
+        StringBuilder resultMessage = new StringBuilder();
+
+        if (gameResult.hasBallCount()) {
+            resultMessage.append(String.format("%d볼 ", gameResult.getBallCount()));
+        }
+        if (gameResult.hasStrikeCount()) {
+            resultMessage.append(String.format("%d스트라이크", gameResult.getStrikeCount()));
+        }
+        if(gameResult.isEndOfGame()) {
+            resultMessage.append("게임이 끝났습니다. 리겜?");
+        }
+
+        return resultMessage.toString();
+    }
+}
